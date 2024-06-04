2e69a71fd6716286ba1ec165ca45c77fa3d5d52b
Marco R <marco.riggio@studenti.polito.it>
2024-06-04 12:50:54 +0200

    feat: Implemented controller for basic p7m extraction

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,11 +1,13 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "p7mExtractor",
-    ) {
-        App()
+fun main() {
+    return application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "P7M Extractor",
+        ) {
+            App()
+        }
     }
 }
